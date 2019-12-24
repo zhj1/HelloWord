e2ff93bb2ea8cc89888c8442a3a560f44bfbc7fc
zhaojie <3298464132@qq.com>
2019-12-24 17:07:43 +0800

    第二次提交

--- src/com/test/Hello.java
+++ src/com/test/Hello.java
@@ -3,6 +3,7 @@ package com.test;
 public class Hello {
     public static  void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("good good study,day day up!");
 
     }
 
